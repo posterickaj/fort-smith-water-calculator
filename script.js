d2cc47a2fed4aafa7c6f8709ee13d1478b037fcb
posterickaj <48775027+posterickaj@users.noreply.github.com>
2026-03-07 17:34:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -54,5 +54,5 @@ document.getElementById("frontageCharge").innerText = frontageCharge.toFixed(2);
 document.getElementById("surcharge").innerText = surcharge.toFixed(2);
 document.getElementById("dividend").innerText = dividend.toFixed(2);
 document.getElementById("newBill").innerText = newBill.toFixed(2);
+  calculate();
 }
-calculate();
